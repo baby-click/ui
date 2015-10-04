50132fa99be877b64b638040ea9dc8dd3c559e38
Aurelius Wendelken <webtobesocial@gmail.com>
2015-10-04 22:18:27 +0200

    updated team page

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,10 @@ var EXTRAS = {
 };
 
 var users = new UserManagement();
+
 users.load(function (err) {
 	console.log('Checking if the user exists');
+
 	users.userExists(USERNAME, function (err, exists) {
 		if (exists) {
 			console.log('User already exists');
@@ -16,6 +18,7 @@ users.load(function (err) {
 		} else {
 			console.log('User does not exist');
 			console.log('Creating the user');
+
 			users.createUser(USERNAME, PASSWORD, EXTRAS, function (err) {
 				console.log('User created');
 				users.close();
@@ -23,52 +26,3 @@ users.load(function (err) {
 		}
 	});
 });
-
-
-
-
-
-
-
-
-
-function updateProfile() {
-
-	/*
-	 * lets assume that your browser can handle "local storage" for the sake of simplicity.
-	 * You should have saves the token to local storage prior to this.
-	 * 
-	 * localStorage.token=myApiResult.token
-	 * 
-	 * myApiResult would be the JSON we received after the login
-	 * 
-	 **/
-	var tokenString = localStorage.getItem("token");
-
-	//simple ajax call to our API
-	$.ajax({
-		url: "http://www.webstreaming.com.ar/api/user/",
-		method: "PUT",
-		headers: {
-			Authorization: tokenString
-		},
-		data: JSON.stringify({
-			skills: "Turn sand into gold",
-			nickname: "God of War"
-		}),
-		statusCode: {
-			404: function () {
-				//If the endpoint is not found, we'll end up in here
-				alert("endpoint not found");
-			},
-			200: function () {
-				//Ok, everything worked as expected
-				alert("worked like a charm");
-			},
-			401: function () {
-				//Our token is either expired, invalid or doesn't exist
-				alert("token not valid or expired");
-			}
-		}
-	});
-}
