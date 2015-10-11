bc5a98c90b798b704ac873d7b0fb17f608b05a0e
Aurelius Wendelken <webtobesocial@gmail.com>
2015-10-11 15:21:01 +0200

    updated user management procedure

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var path = require('path');
-var https = require('https');
+var http = require('http');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
 
@@ -8,44 +8,58 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
+var methodOverride = require('method-override');
+
+var mongoose = require('mongoose');
 
 var passport = require('passport');
-var passportLocal = require('passport-local');
+var LocalStrategy = require('passport-local').Strategy;
 var passportHttp = require('passport-http');
 
+var auth = require('./routes/auth');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+// main config
+app.set('port', process.env.PORT || 1337);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('view options', {
+	layout: false,
 	pretty: true
 });
-
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cookieParser());
 app.use(expressSession({
 	saveUninitialized: false,
-	secret: 'keyboard cat',
-	resave: false
+	secret: 'babyclick secret',
+	resave: true
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
+app.use(methodOverride());
 app.use(passport.initialize());
 app.use(passport.session());
 
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/babyclick');
+
+app.use('/', auth);
 app.use('/', routes);
 app.use('/users', users);
-
-
-
-//=================================================================//
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
