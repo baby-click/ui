8fccf2f528ca50d320547fb51ecc115b09a7e3e3
Aurelius Wendelken <webtobesocial@gmail.com>
2015-10-31 18:03:48 +0100

    added price model

---
+++ models/priceModel.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var priceSchema = new Schema({
+  "price": [{
+    "currency": String,
+    "value": String
+  }],
+  "sale": {
+    "salePrice": [{
+      "currency": String,
+      "value": String
+    }],
+    "saleEndDate": Date
+  },
+  "lastUpdated": Date
+});
+
+module.exports = mongoose.model('price', priceSchema);
